a6e7117a8ad770bee8583618d1dca670cb057d70
Shreya Srivastava <112487189+shreyasrivastava11@users.noreply.github.com>
2026-07-03 18:06:59 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,31 @@
+class MinStack {
+    int[] stack;
+    int i = -1;
+    public MinStack() {
+        stack = new int[10000000];
+    }
+    
+    public void push(int val) {
+        i+=1;
+        stack[i] = val;
+    }
+    
+    public void pop() {
+        i-=1;
+    }
+    
+    public int top() {
+        return stack[i];
+    }
+    
+    public int getMin() {
+        int min = stack[0];
+        for(int j = 0;j<=i;j++){
+            if(min > stack[j]){
+                min = stack[j];
+            }
+        }
+
+        return min;
+    }
+}
